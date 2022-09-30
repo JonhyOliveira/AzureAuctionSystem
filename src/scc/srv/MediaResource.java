d875757a62c78fc11c543aaf3b5c99a224e63763
Joao Oliveira <jfv.oliveira@campus.fct.unl.pt>
2022-09-30 16:45:29 +0100

    :sparkles: feat - blob storage

--- src/scc/srv/MediaResource.java
+++ src/scc/srv/MediaResource.java
@@ -1,5 +1,10 @@
 package scc.srv;
 
+import com.azure.core.util.BinaryData;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
 import jakarta.ws.rs.*;
 import scc.utils.Hash;
 
@@ -17,7 +22,13 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/media")
 public class MediaResource
 {
+	public static final String blobStoreConString = "DefaultEndpointsProtocol=https;AccountName=scc222358001;AccountKey=jml3xMDPLhWu8UYUMYbB6+CFI7qY2BDO6mI9aw9g0oWSShxEGDwuyXQiKHXTAyb1Atk0yX3PJVtG+AStxRY2LQ==;EndpointSuffix=core.windows.net";
+
 	Map<String,byte[]> map = new HashMap<String,byte[]>();
+	BlobContainerClient containerClient = new BlobContainerClientBuilder()
+			.connectionString(blobStoreConString)
+			.containerName("images")
+			.buildClient();
 
 	/**
 	 * Post a new image.The id of the image is its hash.
@@ -25,10 +36,11 @@ public class MediaResource
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
 	public String upload(byte[] contents) {
 		String key = Hash.of(contents);
-		map.put( key, contents);
+		BlobClient blob = containerClient.getBlobClient(key);
+		blob.upload(BinaryData.fromBytes(contents));
 		return key;
 	}
 
@@ -41,21 +53,24 @@ public class MediaResource
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
 	public byte[] download(@PathParam("id") String id) {
 		//throw new ServiceUnavailableException();
-		if (!map.containsKey(id))
+		BlobClient blob = containerClient.getBlobClient(id);
+		if (!blob.exists())
 			throw new ProcessingException("Not found");
-		return map.get(id);
+		return blob.downloadContent().toBytes();
 	}
 
-	private static final String FILE_LIST_FMT = String.format("file id: %%-%ds - %%d bytes long", Hash.HASH_LENGTH);
+	private static final String FILE_LIST_FMT = String.format("file id: <a href= %%1$s>%%-%d1$s</a> - %%d bytes long", Hash.HASH_LENGTH);
 
 	/**
 	 * Lists the ids of images stored.
 	 */
 	@GET
 	@Path("/")
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
 	public String list() {
-		String res = map.keySet().stream().map(s -> String.format(FILE_LIST_FMT, s, map.get(s).length)).collect(Collectors.joining("\n"));
+		String res = containerClient.listBlobs().stream()
+				.map(blobItem -> String.format(FILE_LIST_FMT, blobItem.getName(), blobItem.getProperties().getContentLength()))
+				.collect(Collectors.joining("\n"));
 		if (res.isBlank())
 			return "No content found.";
 		else
