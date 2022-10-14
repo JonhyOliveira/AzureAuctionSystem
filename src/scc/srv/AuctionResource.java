1258ff2ed494291dc389003215efd46f1c97e3a1
André Cordeiro <am.cordeiro@campus.fct.unl.pt>
2022-10-14 12:09:25 +0100

    Create Function Done

--- src/scc/srv/AuctionResource.java
+++ src/scc/srv/AuctionResource.java
@@ -2,10 +2,15 @@ package scc.srv;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import scc.database.Auction;
+import scc.database.AuctionDAO;
 import scc.database.CosmosDBLayer;
+import scc.database.User;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Path("/auction")
 public class AuctionResource {
@@ -18,9 +23,16 @@ public class AuctionResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public String create(Auction auction, @QueryParam(TOKEN_QUERY_PARAM) String authToken)
+    public Auction create(Auction auction, @QueryParam(TOKEN_QUERY_PARAM) String authToken)
     {
-        throw new ServiceUnavailableException();
+        validateAuctionFields(auction, true);
+
+        if(dbLayer.getAuctionByTitle(auction.getTitle()).stream().findAny().isPresent())
+            throw new BadRequestException("Auction already exists");
+
+        AuctionDAO newAuction = new AuctionDAO(auction);
+
+        return dbLayer.putAuction(newAuction).getItem().toAuction();
     }
 
     @PUT
@@ -81,9 +93,36 @@ public class AuctionResource {
         throw new NotSupportedException();
     }
 
-    record Auction(String owner_nickname, List<Bid> bids, List<Question> questions) {}
+    //record Auction(String title, String desc, String photoId, String owner_nickname, long endTime, float minPrice, String aucStatus, List<Bid> bids, List<Question> questions) {}
 
     record Bid(String bidder_nickname, Double amount) {}
 
     record Question(String author_nickname, String text, String answer) {}
+
+    /**
+     * Validates user fields
+     * @throws WebApplicationException if a field contains an invalid
+     */
+    private void validateAuctionFields(Auction auction, boolean requireNonNull) throws WebApplicationException
+    {
+        String error_message = null;
+
+        if (Objects.isNull(auction))
+            error_message = "Auction can not be null";
+        else {
+            if (Objects.requireNonNullElse(auction.getTitle(), requireNonNull ? "" : "a").isBlank())
+                error_message = "Title can not be blank.";
+            if (Objects.requireNonNullElse(auction.getOwner_nickname(), requireNonNull ? "" : "a").isBlank())
+                error_message = "Owner name can not be blank.";
+            if(auction.getEndTime() <= 0)
+                error_message = "Introduce a valid end time.";
+            if(auction.getMinPrice() <= 0)
+                error_message = "Introduce a valid minimum price.";
+            if (Objects.requireNonNullElse(auction.getAucStatus(), requireNonNull ? "" : "a").isBlank())
+                error_message = "Auction status can not be blank.";
+        }
+
+        if (Objects.nonNull(error_message))
+            throw new WebApplicationException(Response.status(Response.Status.BAD_REQUEST.getStatusCode(), error_message).build());
+    }
 }
