ee5bbe780bf5c8b4fac0518475343bb494f0adba
joaooliv <jfv.oliveira@campus.fct.unl.pt>
2022-11-11 13:12:56 +0000

    Added azure functions

---
+++ scc2022-backend/test/scc/data/layers/CosmosDBLayerTest.java
@@ -0,0 +1,27 @@
+package scc.data.layers;
+
+import com.azure.cosmos.implementation.apachecommons.text.translate.AggregateTranslator;
+import com.azure.cosmos.implementation.apachecommons.text.translate.UnicodeUnescaper;
+import com.azure.cosmos.models.PartitionKey;
+import com.azure.cosmos.models.SqlQuerySpec;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CosmosDBLayerTest {
+
+
+    @Test
+    public void ola()
+    {
+        String nickname = "\" or 1=1";
+        String query = "SELECT * FROM users WHERE users.id=\"" + nickname + "\"";
+
+        System.out.println("With:");
+        System.out.println(new PartitionKey("ola"));
+        System.out.println("Without:\n" + query);
+    }
+
+}
---
+++ scc2022-backend/test/scc/data/models/AuctionDAOTest.java
@@ -0,0 +1,21 @@
+package scc.data.models;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuctionDAOTest {
+
+    @Test
+    void testJson() throws JsonProcessingException {
+        AuctionDAO auc = new AuctionDAO("id", "tit", "descri", "epic thumbnail",
+                "me", System.currentTimeMillis() + 1000, 100, false);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        System.out.println(mapper.writeValueAsString(auc));
+    }
+
+}
