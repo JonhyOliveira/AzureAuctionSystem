75c597651902b0535e7ac9eb6ea143a2fdc91e6e
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-11-15 22:44:44 +0000

    Update TimerTriggerFunction.java

--- scc2022-functions/src/scc/functions/TimerTriggerFunction.java
+++ scc2022-functions/src/scc/functions/TimerTriggerFunction.java
@@ -8,17 +8,55 @@ import com.microsoft.azure.functions.*;
  * Azure Functions with Timer trigger.
  * Schedule is: second minute hour ...
  * Apagar auctions
+ * Apagar users da cache
  */
 public class TimerTriggerFunction {
 
     /**
-     * This function will be invoked every minute on the 3rd second
+     * This function will be invoked every 5min to ensure that the auction is deleted
      */
-    @FunctionName("TimerTrigger-Java")
-    public void run(
-        @TimerTrigger(name = "timerInfo", schedule = "3 * * * * *") String timerInfo,
-        final ExecutionContext context
-    ) {
-        context.getLogger().info("Java Timer '"+ timerInfo +"' trigger function executed at: " + LocalDateTime.now());
+    @FunctionName("DeleteAuctionPeriodicFunction")
+    public void deleteAuctionCleanup(
+        @TimerTrigger(
+                    name = "deleleAuctionPeriodicTrigger", 
+                    schedule = "*/5 * * * *") 
+                    String timerInfo,
+        final ExecutionContext context) {
+        
+        CosmosDBLayer db = new CosmosDBLayer();
+        RedisCacheLayer cache = new RedisCacheLayer();
+        
+        /*
+         * Get all auctions that are expired and delete them
+         */
+        List<AuctionDAO> auctions = db.getClosedAuctions();
+        for (AuctionDAO auction : auctions) {
+            //db.deleteAuction(auction.getId()); ------------------------------ provavelmente não é para apagar da base de dados
+            cache.deleteFromCache("auction:"+ auction.getId());
+        }
     }
+
+    /**
+     * This function will be invoked every 5min to ensure that the user is deleted from cache
+     */
+    @FunctionName("DeleteUserPeriodicFunction")
+    public void deleteUserCleanup(
+        @TimerTrigger(
+                    name = "deleleUserPeriodicTrigger", 
+                    schedule = "*/5 * * * *") 
+                    String timerInfo,
+        final ExecutionContext context) {
+        
+        CosmosDBLayer db = new CosmosDBLayer();
+        RedisCacheLayer cache = new RedisCacheLayer();
+        
+        /*
+         * Get all users that are expired and delete them
+         */
+        List<UserDAO> users = cache.getExpiredUsers();
+        for (UserDAO user : users) {
+            cache.deleteFromCache("user:"+ user.getNickname());
+        }
+    }
+
 }
