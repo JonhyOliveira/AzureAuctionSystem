a61a75ed08c0fcaee9134d65fd2f6356e2635129
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-11-15 21:40:52 +0000

    Create TimeTriggerFunction.java

---
+++ scc2022-functions/src/scc/functions/TimeTriggerFunction.java
@@ -0,0 +1,24 @@
+package scc.functions;
+
+import java.time.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+/**
+ * Azure Functions with Timer trigger.
+ * Schedule is: second minute hour ...
+ * Apagar auctions
+ */
+public class TimeTriggerFunction {
+
+    /**
+     * This function will be invoked every minute on the 3rd second
+     */
+    @FunctionName("TimerTrigger-Java")
+    public void run(
+        @TimerTrigger(name = "timerInfo", schedule = "3 * * * * *") String timerInfo,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java Timer '"+ timerInfo +"' trigger function executed at: " + LocalDateTime.now());
+    }
+}
