3901932e1bc246406145dd6d6b598b9f612e6ac2
André Cordeiro <am.cordeiro@campus.fct.unl.pt>
2022-11-19 17:26:39 +0000

    Implemented deleteDandglingImages

--- scc2022-functions/src/scc/functions/GarbageCollectionFunctions.java
+++ scc2022-functions/src/scc/functions/GarbageCollectionFunctions.java
@@ -1,13 +1,16 @@
 package scc.functions;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.azure.cosmos.implementation.apachecommons.lang.NotImplementedException;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
+import scc.data.layers.BlobStorageLayer;
 import scc.data.layers.CosmosDBLayer;
 
 /**
@@ -43,8 +46,24 @@ public class GarbageCollectionFunctions {
     public void deleteDanglingImages( // TODO change schedule to "0 0 0 */3 * *" when this is working
             @TimerTrigger(name = "GC-DanglingImagesTrigger", schedule = "0 0 0 * * *") String timerInfo,
             final ExecutionContext context) {
-        // TODO
-        throw new NotImplementedException(":(");
+
+        CosmosDBLayer db = CosmosDBLayer.getInstance();
+        BlobStorageLayer bs = BlobStorageLayer.getInstance();
+
+        List<String> imagesFromUsers = db.getAllImagesFromTable("users").collect(Collectors.toList());
+        List<String> imagesFromAuctions = db.getAllImagesFromTable("auctions").collect(Collectors.toList());
+        Set<String> allImages = new HashSet<>(imagesFromUsers);
+        allImages.addAll(imagesFromAuctions);
+
+        List<String> imagesFromBS = bs.listFiles().collect(Collectors.toList());
+
+        for(String imgId : imagesFromBS){
+            if(!allImages.contains(imgId))
+                bs.deleteBlob(imgId);
+        }
+
+        context.getLogger().info("All images that were not being used were removed from blob storage.");
+        //throw new NotImplementedException(":(");
     }
 
 }
