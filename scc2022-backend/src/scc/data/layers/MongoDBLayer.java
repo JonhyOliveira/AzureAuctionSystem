4ab91f439afbfeae280d879324294b04ea89fe47
romaomcosta98 <rlm.costa@campus.fct.unl.pt>
2022-12-03 12:24:18 +0000

    Starting MongoDBLayer Methods

--- scc2022-backend/src/scc/data/layers/MongoDBLayer.java
+++ scc2022-backend/src/scc/data/layers/MongoDBLayer.java
@@ -125,7 +125,13 @@ public class MongoDBLayer implements DBLayer {
     @Override
     public Stream<UserDAO> getUsers() {
         init();
-        return null;
+        return users.find().map(Document::toJson).map(s -> {
+            try {
+                return mapper.readValue(s, UserDAO.class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }).toStream();
     }
 
     @Override
@@ -143,7 +149,15 @@ public class MongoDBLayer implements DBLayer {
     @Override
     public Optional<AuctionDAO> updateAuction(AuctionDAO auction) {
         init();
-        return Optional.empty();
+        return Optional.ofNullable(auctions.findOneAndReplace(Filters.eq("_id", auction.getId()), Document.parse(mapper.writeValueAsString(auction))))
+                .map(Document::toJson)
+                .map(s -> {
+                    try {
+                        return mapper.readValue(s, AuctionDAO.class);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
     @Override
@@ -168,6 +182,7 @@ public class MongoDBLayer implements DBLayer {
     public Optional<BidDAO> putBid(BidDAO bid) {
         init();
         return Optional.empty();
+        
     }
 
     @Override
